3b87835dfacfce03aaf1f2df758e925b087eca29
wildanunhasanah.github.io <wildanunhasanah60@gmail.com>
2025-09-11 16:43:17 +0700

    DataDiri

---
+++ src/modul_1/tipedata/DataDiri.java
@@ -0,0 +1,19 @@
+package modul_1.tipedata;
+
+public class DataDiri {
+    public static void main(String[] args) {
+        String nama, alamat;
+        int usia;
+        double tinggi;
+
+        nama = "Wilda";
+        alamat = "Bayu";
+        tinggi = 150;
+        usia = 5;
+
+        System.out.println("name: " + nama);
+        System.out.println("alamat: " + alamat);
+        System.out.println("tinggi: " + tinggi);
+        System.out.println("usia: " + usia);
+    }
+}
